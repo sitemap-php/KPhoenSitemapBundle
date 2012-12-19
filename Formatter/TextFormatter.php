5666b9920124f3758bc2ff49b7426210402c243a
kphoen <geek63@gmail.com>
2012-12-19 12:57:43 +0000

    Add: TextFormatter (see http://www.sitemaps.org/protocol.html#otherformats)

---
+++ Formatter/TextFormatter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace KPhoen\SitemapBundle\Formatter;
+
+use KPhoen\SitemapBundle\Entity\Url;
+
+
+/**
+ * Sitemaps formatted using this class will contain only one URL per line and
+ * no other information.
+ *
+ * @see http://www.sitemaps.org/protocol.html#otherformats
+ */
+class TextFormatter implements FormatterInterface
+{
+    public function getSitemapStart()
+    {
+        return '';
+    }
+
+    public function getSitemapEnd()
+    {
+        return '';
+    }
+
+    public function formatUrl(Url $url)
+    {
+        return $url->getLoc() . "\n";
+    }
+}
