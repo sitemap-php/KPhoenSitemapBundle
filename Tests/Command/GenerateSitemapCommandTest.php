d5477cc414c59b97c9f1f0a9a1518645b4d838b2
kphoen <geek63@gmail.com>
2012-12-21 08:17:12 +0000

    Add: tests for the command

---
+++ Tests/Command/GenerateSitemapCommandTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace KPhoen\SitemapBundle\Tests\Controller;
+
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+use KPhoen\SitemapBundle\Command\GenerateSitemapCommand;
+
+
+class GenerateSitemapCommandTest extends WebTestCase
+{
+    public function testSitemapNbUrls()
+    {
+        $kernel = $this->createKernel();
+        $kernel->boot();
+
+        $application = new Application($kernel);
+        $application->add(new GenerateSitemapCommand());
+
+        $command = $application->find('sitemap:generate');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName()));
+
+        $this->assertRegExp('`http://www.google.fr`', $commandTester->getDisplay());
+        $this->assertRegExp('`http://github.com`', $commandTester->getDisplay());
+    }
+}
